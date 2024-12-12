e97d65a33a5dc98784affb2f1df1dbb3cc203727
Ramin Gahramanzada <119501802+RaminGahramanzada@users.noreply.github.com>
2024-12-12 14:53:48 +0400

    LinearTraversal

---
+++ src/main/java/basics/arrays/LinearTraversal.java
@@ -0,0 +1,183 @@
+package basics.arrays;
+
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class LinearTraversal {
+
+    //1.Find the sum of all elements in an array
+    public int findSumOfArray(int [] arr){
+        int sum =0;
+        for(int element : arr){
+            sum+=element;
+        }
+        return sum;
+    }
+
+    //2.Find the smallest element in an array
+    public int findTheSmallestNumber(int [] arr){
+        int min = arr[0];
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min = arr[i];
+            }
+        }
+
+        return min;
+    }
+
+    //3.Count the even numbers in an array
+    public int countEvenNum(int arr[]){
+        int count =0;
+
+        for(int element : arr){
+            if(element%2==0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    //4.Find the sum of all positive numbers in an array
+    public int sumAllPosNum(int arr[]){
+        int sum =0;
+
+        for(int element : arr){
+            if(element>0){
+                sum+=element;
+            }
+        }
+
+        return sum;
+    }
+
+    //5.Count the negative numbers in an array
+    public int countAllNegNum(int []arr){
+        int count=0;
+
+        for(int element : arr){
+            if(element<0){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    //6.6	Determine how many times a specific element appears
+    public int countFrequencyTargetNum(int target,int arr[]){
+        int count =0;
+
+        for(int element : arr){
+            if(element==target){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    //7.Calculate the product of all odd numbers
+    public int productOfAllOddNum(int arr[]){
+        int product =1;
+
+        for(int element : arr){
+            if(element%2!=0){
+                product*=element;
+            }
+        }
+
+        return product;
+    }
+
+    //8.Check if a specific element exists in an array
+    public boolean isExistsTarget(int target,int arr[]){
+
+        for(int element : arr){
+            if(target==element){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    //9.Find difference between max and min elements
+    public int findDifferenceMaxAndMin(int arr[]){
+        int diff =0;
+        int max=arr[0];
+        int min =arr[0];
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min){
+                min =arr[i];
+            }
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        diff = max-min;
+
+        return diff;
+    }
+
+    //10.Find the index of the largest element
+    public int findIndexOfLargstElmn(int arr[]){
+        int index =0;
+        int max =arr[0];
+
+        for(int element :arr){
+            if(max<element){
+                max = element;
+            }
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==max){
+                index=i;
+            }
+        }
+
+        return index+1;//array's first index 0 but I want 1
+    }
+
+    //11.Replace all ones with zero
+    public int [] replaceOneToZero(int[] arr){
+        int [] replacedArr = new int[arr.length];
+
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==1){
+                replacedArr[i]=0;
+            }else{
+                replacedArr[i] =arr[i];
+            }
+        }
+        return replacedArr;
+
+    }
+
+
+    public Map<Integer,Integer> frequencyForEachElement(int arr[]){
+
+        Map<Integer,Integer> list = new HashMap<>();
+        for(int element : arr){
+            list.put(element,list.getOrDefault(element,0)+1);
+        }
+        return list;
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/test/java/basics/arrays/LinearTraversalTest.java
@@ -0,0 +1,117 @@
+package basics.arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+class LinearTraversalTest {
+    LinearTraversal lt = new LinearTraversal();
+
+    @Test
+    void findSumOfArray() {
+        int [] arr = {1,2,3,4,5};
+        int actual = lt.findSumOfArray(arr);
+        int expected= 15;
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void findTheSmallestNumber(){
+        int [] arr = {4,2,3,9,5};
+        int actual = lt.findTheSmallestNumber(arr);
+        int expected = 2;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void countEvenNum() {
+        int arr[] ={6,1,3,8,9,10};
+        int actual = lt.countEvenNum(arr);
+        int expected = 3;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void sumAllPosNum() {
+        int arr[] ={6,1,3,8,9,10};
+        int actual = lt.sumAllPosNum(arr);
+        int expected = 37;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void countAllNegNum() {
+        int arr[] ={6,1,-3,8,-9,10};
+        int actual = lt.countAllNegNum(arr);
+        int expected = 2;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void countFrequencyTargetNum() {
+        int arr[] ={6,1,5,8,5,5};
+        int target =5;
+        int actual = lt.countFrequencyTargetNum(target,arr);
+        int expected = 3;
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void productOfAllOddNum() {
+        int [] arr = {1,2,3,4,5};
+        int actual = lt.productOfAllOddNum(arr);
+        int expected= 15;
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void isExistsTarget() {
+        int target = 4;
+        int [] arr ={1,2,3,4,5};
+        boolean actual = lt.isExistsTarget(target,arr);
+        boolean expected =true;
+
+        assertEquals(expected,actual);
+    }
+
+    @Test
+    void findDifferenceMaxAndMin() {
+        int [] arr = {1,2,3,4,5};
+        int actual = lt.findDifferenceMaxAndMin(arr);
+        int expected= 4;
+    }
+
+    @Test
+    void findIndexOfLargstElmn() {
+        int [] arr = {1,2,9,4,5};
+        int actual =3 ;
+        int expected= lt.findIndexOfLargstElmn(arr);
+    }
+
+    @Test
+    void replaceOneToZero() {
+        int arr [] ={1,1,4,2,1,6};
+        int [] actual = lt.replaceOneToZero(arr);
+        int [] expected = {0,0,4,2,0,6};
+        assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    void frequencyForEachElement() {
+        int []  actual = {1,2,1,2,3,4,5,4,6};
+        Map<Integer, Integer> expected = new HashMap<>();
+        expected.put(1,2);
+        expected.put(2,2);
+        expected.put(3,1);
+        expected.put(4,2);
+        expected.put(5,1);
+        expected.put(6,1);
+        assertEquals(expected,lt.frequencyForEachElement(actual));
+
+    }
+}
